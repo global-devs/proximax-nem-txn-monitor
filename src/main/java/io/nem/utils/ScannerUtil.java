a9b334c21fe4298df6eed670b01a430dce32cee0
Thu Cao <thu.cao@fdssoft.com>
2018-09-05 15:23:45 +0700

    Add support shutdown server

--- src/main/java/io/nem/utils/ScannerUtil.java
+++ src/main/java/io/nem/utils/ScannerUtil.java
@@ -2,29 +2,39 @@ package io.nem.utils;
 
 import java.util.Scanner;
 
-
-
 /**
  * The Class ScannerUtil.
  */
 public class ScannerUtil {
 
-	/**
-	 * exit when enter string "exit".
-	 */
-	public static void monitorExit() {
-		Scanner scanner = new Scanner(System.in);
-		try{
-	        while (true) { 
-		        String line = scanner.nextLine();
-		        if("exit".equals(line)){
-		        	break;
-		        }
-	        }
-		} catch (Exception ex) {
-			
-		} finally {
-			scanner.close();
-		}
-	}
+    public static boolean bShutdown = false;
+
+    /**
+     * exit when enter string "exit".
+     */
+    public static void monitorExit() {
+        bShutdown = false;
+        Scanner scanner = new Scanner(System.in);
+        try {
+            while (true) {
+                String line = scanner.nextLine();
+                if (ScannerUtil.isShutdown() || "exit".equals(line)) {
+                    break;
+                }
+
+            }
+        } catch (Exception ex) {
+
+        } finally {
+            scanner.close();
+        }
+    }
+
+    public static boolean isShutdown() {
+        return bShutdown;
+    }
+
+    public static void destroy() {
+        bShutdown = true;
+    }
 }
